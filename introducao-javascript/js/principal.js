481c2339fca3b19eafccdd186ede29d320e85ee4
Vinicius Chagas <vmc.mart@gmail.com>
2021-12-18 21:19:21 -0300

    Atulizando o README.me e começando o primeiro projeto introducao-javascript.

---
+++ introducao-javascript/js/principal.js
@@ -0,0 +1,4 @@
+		/* [#1] Mudando o texto dentro de um H1 com ID específico  */
+		var titleH1 = document.querySelector(".titleH1");
+		titleH1.textContent = "Vinicius Chagas";
+        console.log("Título alterado para "+titleH1.textContent+"!");

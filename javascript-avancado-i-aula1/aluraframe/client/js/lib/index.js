7246f2d373cf65cd9d311c8951e77bd8e0f1ebf0
Vinicius Chagas <vmc.mart@gmail.com>
2022-01-02 19:42:04 -0300

    Inserindo valores na tabela

---
+++ javascript-avancado-i-aula1/aluraframe/client/js/lib/index.js
@@ -0,0 +1,23 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+];
+
+var form = document.querySelector('.form');
+var tBody = document.querySelector('table tbody');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var tr = document.createElement('tr');
+    campos.forEach(function (campo) {
+        var td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+    var tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+    tr.appendChild(tdVolume);
+    tBody.appendChild(tr);
+    form.reset();
+});

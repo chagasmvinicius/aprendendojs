2cf037ea6f5f8b3b559d251700af4a945337b1ae
Vinicius Chagas <vmc.mart@gmail.com>
2022-01-05 11:23:39 -0300

    Utilizando programação defensiva no método get negociacoes()

--- javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
+++ javascript-avancado-i-aula1/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,6 +1,9 @@
 /*
 1 - Criação da classe "ListaNegociacoes" dentro de app/models. Essa classe é responsável por criar uma lista com as negociações inputadas e conter
 os métodos para adicionar novas negociações (push no array) e um get para retornar a lista atualizada;
+2 - Inserir programação defensiva para blindar o método "get negociacoes()" para retornar uma cópia da lista original (return [].concat(this._negociacoes)). 
+Dessa forma, se quiserem, através do console, limpar os dados do array com ".length = 0" ou utilizar diretamente o ".push" para adiconar novas negociações no array,
+não irá alterar a lista original, pois estamos enviando uma cópia dela nesse método;
 */
 
 class ListaNegociacoes {
@@ -14,6 +17,6 @@ class ListaNegociacoes {
     }
 
     get negociacoes() {
-        return this._negociacoes;
+        return [].concat(this._negociacoes);
     }
 }

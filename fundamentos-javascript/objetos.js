815cb08064233e81aa0dcfb1e8072288aa4cc3e4
Vinicius Chagas <vmc.mart@gmail.com>
2022-01-08 17:14:19 -0300

    Acessando propriedades/chaves de um objeto a partir de chaves armazenadas em uma lista

--- fundamentos-javascript/objetos.js
+++ fundamentos-javascript/objetos.js
@@ -4,27 +4,32 @@ let identidades = [
     {
         nome: 'Vinicius',
         idade: 28,
-        email: 'vinicius@email.com'
+        email: 'vinicius@email.com',
+        cpf: '12344278820'
     },
     {
         nome: 'Karize',
         idade: 24,
-        email: 'karize@email.com'
+        email: 'karize@email.com',
+        cpf: '72384378843'
     },
     {
         nome: 'Nilvaldo',
         idade: 61,
-        email: 'nilvaldo@gmail.com'
+        email: 'nilvaldo@gmail.com',
+        cpf: '65344528990'
     },
     {
         nome: 'Soraya',
         idade: 63,
-        email: 'soraya@gmail.com'
+        email: 'soraya@gmail.com',
+        cpf: '71448878902'
     },
     {
         nome: 'Irhael',
         idade: 17,
-        email: 'irhael@gmail.com'
+        email: 'irhael@gmail.com',
+        cpf: '22377278121'
     }
 ];
 
@@ -33,8 +38,26 @@ identidades.forEach(identidade => {
         Nome: ${identidade.nome};
         Idade: ${identidade.idade};
         E-mail: ${identidade.email};
+        CPF: ${identidade.cpf.substring(0, 3)}******${identidade.cpf.substring(9, identidade.cpf.length)}
     `);
 });
+
+/* Acessando cada chave do objeto a partir de uma lista que contém todas as chaves */
+/*
+A expressão identidades[i][chaves[c]] faz com que a propriedade de indice "i" do objeto seja o
+valor do indice "c" do array chaves. Dessa forma.:
+> [chaves[c]]: identidades[i][chaves[c]]
+*/
+
+const chaves = ['nome', 'idade', 'email', 'cpf'];
+
+for (let i = 0; i < identidades.length; i++) {
+    for (let c = 0; c < chaves.length; c++) {
+        console.log(`
+        ${[chaves[c]]}: ${identidades[i][chaves[c]]}
+    `);
+    }
+}
 
 /*-----------------------------------------------------------------------------*/
 
